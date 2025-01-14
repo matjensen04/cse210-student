2977ea37d1e2809d31535b492f6db121e27a5c8f
Matthew Jensen <jen220757@byui.edu>
2025-01-13 17:20:26 -0700

    Added code to ask user for first and last name, then print it out in the order of last name, then first and last name.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string Fname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string Lname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {Lname}, {Fname} {Lname}.");
     }
 }
