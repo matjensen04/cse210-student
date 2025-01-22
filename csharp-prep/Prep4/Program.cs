0d99313b92f9810d2f42f58efe99fd392be75446
Matthew Jensen <jen220757@byui.edu>
2025-01-21 21:16:37 -0700

    Added code where I creat an int list and ask the user for numbers. The list stores all numbers then calculates the sum, average, and max.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers =new List<int>();
+
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number. Type 0 to quit: ");
+            string response = Console.ReadLine();
+            number = int.Parse(response);
+            
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number2 in numbers)
+        {
+            sum += number2;
+        }
+
+        Console.WriteLine($"The sume is: {sum}");
+        
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number3 in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+    
+        Console.WriteLine($"The max is: {max}");
     }
+
+    
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,82 @@
 using System;
+using System.Runtime.ConstrainedExecution;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        //short x = 32700;
+        //Console.WriteLine(x);
+
+        //for (int i = 0; i < 100; i++)
+        //{
+            //x++;
+            //Console.WriteLine(x);
+        //}
+
+        //Console.Write("Enter first name: ");
+        //string firstname = Console.ReadLine();
+        //Console.WriteLine($"Hello {firstname}!");
+
+        //Multiple ways to convert, I prefer Parse
+        //string age_string = "34";
+        //int age2 = int.Parse(age_string);
+        //int age3 = Convert.ToInt32(age2);
+        //Console.WriteLine($"{age3}");
+
+        //int age = 20;
+        //bool canVote = false;
+        //if (age >= 18)
+        //{
+        //    canVote = true;
+        //}
+        //else
+        //{
+        //    canVote = false;
+        //}
+        //Console.WriteLine(canVote);
+
+        // Python version vote = Trueif age >= 18 else False
+        //bool vote = (age >= 18) ? true : false;
+        //string vote_string = vote ? "Yes" : "No";
+
+        // --------------------------------------------
+        // AND , OR, And Not operators
+        // &&, ||, !
+        
+        //bool citizen = false;
+        //if (age >= 18 && citizen);
+
+        //------------------------------
+        //While loops
+
+        //int count = 0;
+        //while (count < 10);
+        //{
+        //    Console.WriteLine($"Count is {count}");
+        //    count++;
+        //}
+
+        //string name;
+        //do
+        //{
+        //    Console.WriteLine("Enter your name: ");
+        //    name = Console.ReadLine();
+        //} while (name.Length < 5);
+
+        //Lists
+
+        List<int> numbers = new List<int>();
+        Console.WriteLine(numbers.Count);
+
+        numbers.Add(1);
+        numbers.Add(10);
+        numbers.Add(100);
+        numbers.Add(1000);
+
+        Console.WriteLine(string.Join(", ", numbers));
+        numbers.Reverse();
+        Console.WriteLine(string.Join(", ", numbers));
+
     }
 }
